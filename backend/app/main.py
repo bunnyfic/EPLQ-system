af3fe80b0eed675944c1c9f6816432bb4a4eedb6
maya kamat <savagesuga257@gmail.com>
2026-07-26 23:26:04 +0530

    Update main.py

--- backend/app/main.py
+++ backend/app/main.py
@@ -19,7 +19,7 @@ app = FastAPI(title="EPLQ API")
 origins = [
     "http://localhost:5173",
     "http://127.0.0.1:5173",
-    "https://eplq-system-eta.vercel.app/",
+    "https://eplq-system-eta.vercel.app",
 ]
 app.add_middleware(
     CORSMiddleware,
